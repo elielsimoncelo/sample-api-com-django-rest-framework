339915a8e8dc6af1a77fcf3fc34a77c7bfc43a0b
Eliel Sousa <sousaeliel@gmail.com>
2021-05-16 18:25:24 -0300

    feat: adicionando viewsets, routers e serializers

--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,20 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from app.models import Aluno, Curso
+from app.serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = { 'id': 1, 'nome': 'aluno 1' }
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos(as)"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+# from django.http import JsonResponse
+
+# def alunos(request):
+#     if request.method == 'GET':
+#         aluno = { 'id': 1, 'nome': 'aluno 1' }
+#         return JsonResponse(aluno)
